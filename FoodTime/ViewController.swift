4d33b25bba89ff980f2cfcbeaa111a1cb0655324
Christoper Jonathan <christoper21jo@gmail.com>
2017-11-29 08:52:04 +0700

    Initial Commit

---
+++ FoodTime/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTime
+//
+//  Created by Christoper Jonathan on 28/11/17.
+//  Copyright © 2017 binus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
